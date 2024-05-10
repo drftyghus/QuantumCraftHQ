58191e074cc4e42586b7aed4624168ec3d430a19
drftyghus <gs1kasar642@hotmail.com>
2024-05-11 02:38:27 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const person = { name: "Alice", age: 30 };
-if (person.hasOwnProperty("age")) {
-  console.log("The person has an age property");
+function rightSideView(root) {
+  if (!root) return [];
+  const result = [];
+  let level = 0;
+  const traverse = (node, level) => {
+    if (!node) return;
+    if (result[level] === undefined) result[level] = node.val;
+    traverse(node.right, level + 1);
+    traverse(node.left, level + 1);
+  };
+  traverse(root, level);
+  return result;
 }
